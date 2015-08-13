f3f2954e9a838af03183c4172f30c3dcbca03c0b
Jason Larke <jasonlarke@live.com.au>
2015-08-13 17:22:40 +0800

    Moved core modules into edi folder

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 "use strict";
 
-var di  = require("node-di"),
-    nei = require("./lib/core")(di); 
+var di  = require("node-di");
 
 var DEFAULT_CONFIGURATION = {
     strictDi: true
 };
+
+// Register the core components
+require("./lib/edi/core")(di);
 
 module.exports = {
     module:	    di.module,
@@ -20,21 +22,22 @@ module.exports = {
         
         modules = modules || [];
         
-        // Create a 'value' provider for the express component so we can inject it
+        // Create a 'value' provider for the express component so we can inject it into other services
         modules.unshift(["$provide", function($provide) {
             $provide.value("$express", express);
         }]);
         
         // force load the core modules first
-        modules.unshift("nei");
+        modules.unshift("edi");
 
         // create the injector
         var injector = di.injector(modules, config.strictDi);
         
-        // add a custom module to hook into express's middleware
-        injector.invoke([ "$injector", "$route", function($injector, $route) {
-            $injector.invoke($route.$$resolve);
-        }]);
+        // add an injected hook into express's middleware
+        injector.invoke(injector.get("$route").$$resolve);
+        //injector.invoke([ "$injector", "$route", function($injector, $route) {
+        //    $injector.invoke($route.$$resolve);
+        //}]);
         
         return injector;
     }
